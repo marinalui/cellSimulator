ec5457d0680daec0cc3bb9568559393bca8fc774
Ashley Miller <akm3192@rit.edu>
2024-03-02 15:07:23 -0500

    Tissue class in progress :)

---
+++ src/Tissue.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tissue {
+    private ArrayList<Cell> baseCells;
+    public Tissue(int baseCellNum){
+        baseCells = new ArrayList<Cell>();
+        for(int i=0;i<baseCellNum;i++){
+            baseCells.add(new Cell());
+        }
+    }
+    public void execute(int timePassed){
+        for(Cell currentCell: baseCells){}
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many base cells would you like?");
+
+    }
+}
