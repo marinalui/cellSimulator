384521e69c0279c8c2bc3d8bcb30460ea76c2b38
Ashley Miller <akm3192@rit.edu>
2024-03-02 15:54:54 -0500

    Tissue class in progress :)

--- src/Checkpoint.java
+++ src/Checkpoint.java
@@ -1,3 +1,3 @@
 public class Checkpoint {
-    public Checkpoint(Phases){}
+    public Checkpoint(Phases phase){}
 }
--- src/Phases.java
+++ src/Phases.java
@@ -1,7 +1,10 @@
 public enum Phases {
     G1(20), S(35),G2(50), M(60);
-    public final int timePeriod;
+    private final int timePeriod;
     private Phases( int timePeriod){
         this.timePeriod= timePeriod;
+    }
+    public int getTime(){
+        return timePeriod;
     }
 }
