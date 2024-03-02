3f105a42da12d614c8cc5382c0dc52827d3d9770
Ashley Miller <akm3192@rit.edu>
2024-03-02 16:17:39 -0500

    checkpoint in progress

--- src/Checkpoint.java
+++ src/Checkpoint.java
@@ -1,3 +1,17 @@
 public class Checkpoint {
-    public Checkpoint(Phases phase){}
+    private Phases phase;
+    private Cell currentCell;
+    public Checkpoint(){
+
+    }
+    public boolean test(Cell currentCell){
+        this.currentCell = currentCell;
+        phase = this.currentCell.getPhase();
+        if (phase==phase.G1){
+            return true;
+        }
+        else if(phase==phase.G2){return true;}
+        else {return false;}
+
+    }
 }
